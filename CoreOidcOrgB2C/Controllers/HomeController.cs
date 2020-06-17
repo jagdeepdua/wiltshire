e913237ea8acefe5f170bfbaa246331e4ae0440a
jagdeepdua <61287839+jagdeepdua@users.noreply.github.com>
2020-06-17 22:06:37 +0100

    Add files via upload

---
+++ CoreOidcOrgB2C/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using CoreOidcOrgB2C.Models;
+
+namespace CoreOidcOrgB2C.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = Startup.IsUsingB2C ? "B2C" : "Org";
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Claims()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
