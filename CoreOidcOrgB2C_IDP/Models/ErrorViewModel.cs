e54f01ec400ffd8bb3986d46b1c99d0a28d8a5bb
jagdeepdua <61287839+jagdeepdua@users.noreply.github.com>
2020-06-17 22:03:22 +0100

    Add files via upload

---
+++ CoreOidcOrgB2C_IDP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreOidcOrgB2C_IDP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
